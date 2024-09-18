cf2468d34fdc4b017c98d38978c3d605e2b58893
Braison Wabwire <braisonwabwire2003@gmail.com>
2024-09-18 11:23:54 +0300

    creating a dataframe

---
+++ sql_like_joins.py
@@ -0,0 +1,9 @@
+import pandas as pd
+students_data=pd.DataFrame({
+    'roll_number':[101,102,103,104,105],
+    'names':['Braison','calvin','james','elijah','mathew'],
+    'grades':['A','B','A','c','B'],
+    'marks':[28,22,27,14,18],
+    'city':['nakuru','nairobi','nakuru','mombasa','kericho']
+})
+print(students_data)
